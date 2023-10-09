72c4eacad7027e5a6a6b9b1de0b513e97cf4e8f6
Aleksandr Zhdankov <alexandr0751@yandex.ru>
2023-10-09 21:17:10 +0300

    init

---
+++ netology_HW_1.1.sql
@@ -0,0 +1,19 @@
+-- Oracle
+
+CREATE TABLE PERSONS (
+    name varchar,
+    surname varchar,
+    age varchar,
+  	phone_number varchar,
+  	city_of_living varchar,
+  	PRIMARY KEY(name, surname, age)
+ );
+ 
+select name, surname
+from PERSONS
+where city_of_living = 'MOSCOW';
+
+select *
+from PERSONS
+where age > 27
+order by age desc;
